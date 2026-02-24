52c5cc7ee0ba6664e7b37461a66b3e13ceadc0c1
Oliver Alcaraz <s242289@students.aie.edu.au>
2026-02-24 11:48:29 +1100

    BABIES FREEZE FINALLY

---
+++ Source/TowerOfBaby/Private/Baby.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TowerOfBaby/Public/Baby.h"
+
+#include "Engine/SkeletalMeshSocket.h"
+#include "Misc/MapErrors.h"
+
+
+// Sets default values
+ABaby::ABaby()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("Body"));
+}
+
+// Called when the game starts or when spawned
+void ABaby::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABaby::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TowerOfBaby/Public/Baby.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Baby.generated.h"
+
+UCLASS()
+class TOWEROFBABY_API ABaby : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaby();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	
+public:
+	UPROPERTY(BlueprintReadWrite)
+	USkeletalMeshComponent* SkeletalMesh; 
+};
